c434a5cb21d417c30ed27145587325e4cdbaadc7
jcchakradhar <jcchakradharreddy@gmail.com>
2025-06-02 22:47:30 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from jsonwebtoken;
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;//comes from signup
     const hashedPassword=bcryptjs.hashSync(password,10);//10 is a string which combines with our hashed password
@@ -15,4 +16,28 @@ export const signup=async(req,res,next)=>{
         next(errorHandler(550,'error from the function'));
     }
     
+};
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validuser=await User.findOne({email:email});
+        if(!validuser) return next(errorHandler(404,'user not found'));
+        //check password
+        const validpassword=bcryptjs.compareSync(password,validuser.password);
+        //if password is not valid return error using middleware
+        return next(errorHandler(404,'Wrong Credentials'));
+        //If email exists and password matches, sign-in is successful!
+        //create token
+        const token=jwt.sign({id:validuser._id},process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc;
+        res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+        //remove password as validuserr sends whole its body
+
+
+    }catch(error){
+
+    }
 };
