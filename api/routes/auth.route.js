56e1e09467b248cd4dbe97df42465034b0a10c70
jcchakradhar <jcchakradharreddy@gmail.com>
2025-06-23 16:51:25 +0530

    added signout functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { signin, signup ,google} from '../controllers/auth.controller.js';
+import { signin, signup ,google, signOut} from '../controllers/auth.controller.js';
 const router=express.Router();
-router.post("/signup",signup);
-router.post("/signin",signin);
-router.post("/google",google);
+router.post("/signup",signup);//creates new user acnt in database so post
+router.post("/signin",signin);//sends sensitive data in req.body ,creates session,so post
+router.post("/google",google);//Creates session, handles tokens securely
+router.get("/signout",signOut);
 export default router;
