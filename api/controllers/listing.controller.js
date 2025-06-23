4514af8680a352dcbc570748550f87a2ce2e8974
jcchakradhar <jcchakradharreddy@gmail.com>
2025-06-23 18:29:43 +0530

    created listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -5,7 +5,9 @@ export const createListing=async(req,res,next)=>{
     try{
         const listing =await Listing.create(req.body);//req.body comes from browser
         return res.status(201).json(Listing);
+        console.log("ok");
     }catch(error){
         next(error);
+
     }
 }
