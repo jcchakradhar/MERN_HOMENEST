8be40a607d365e8e206a33052c3285c2faa1ff60
jcchakradhar <jcchakradharreddy@gmail.com>
2025-06-23 16:37:02 +0530

    added delete user functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { test, updateUser } from '../controllers/user.controller.js';
+import { test, updateUser,deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router=express.Router();
 router.get('/test',test);// When a GET request is made to /test, call the 'test' function
 router.post('/update/:id',verifyToken ,updateUser);
+router.delete('/delete/:id',verifyToken ,deleteUser);
 export default router;
